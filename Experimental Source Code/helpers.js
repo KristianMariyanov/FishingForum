c2b8a43006d57272bee769571a6ec1d3892e1845
Yana Slavcheva <yana.slavcheva@gmail.com>
2014-12-03 12:23:02 +0200

    Register functionality refactored. We have now the script in register.js outside ot the HTML and seperated from the helpers.js

--- Experimental Source Code/helpers.js
+++ Experimental Source Code/helpers.js
@@ -22,64 +22,6 @@ function logOut(){
     localStorage.removeItem("sessionToken");
 }
 
-var registerUser = function (){
-        var username= $("#username").val();
-        var password= $("#password").val();
-        var email= $("#email").val();
-        var firstName = $("#firstName").val();
-        var lastName= $("#lastName").val();
-        var aboutMe= $("#aboutMe").val();
-
-        var createdUserId = "";
-
-        $.ajax({
-            method: "POST",
-            url: 'https://api.parse.com/1/users',
-            contentType: "application/json",
-            data: JSON.stringify({
-                "username":username,
-                "password":password,
-                "firstName": firstName,
-                "lastName":lastName,
-                "rating":0,
-                "email":email,
-                "aboutMe":aboutMe
-            }),
-            success: function(data){
-                setRole(data);
-            },
-            error: function(err){
-                console.log(err);
-            }
-        });
-    };
-
-function setRole(data){
-    $.support.cors = true;
-    $.ajax({
-        method: "PUT",
-        contentType: "application/json",
-        data: JSON.stringify({
-            "users":
-            {
-                "__op":"AddRelation",
-                "objects":[
-                    {   "__type":"Pointer",
-                        "className":"_User",
-                        "objectId":data.objectId
-                    }]
-            }}),
-        url: 'https://api.parse.com/1/classes/_Role/fMLotQQa9j',
-        success: function(data){
-            console.log('Successfully assigned a role!');
-        },
-        error: function(err){
-            console.log("Fail!!!! --");
-            console.log(err);
-        }
-    });
-}
-
 function getUserByObjectId(objectId){
     $.ajax({
         method: "GET",
