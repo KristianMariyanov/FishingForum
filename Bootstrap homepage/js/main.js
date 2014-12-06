ce3adfd7f9f26c56dca81bec590b7f5f7491f615
JiVeNcE <jivence10@gmail.com>
2014-12-06 12:27:45 +0200

    forum page and user page uploads

--- Bootstrap homepage/js/main.js
+++ Bootstrap homepage/js/main.js
@@ -2,7 +2,8 @@
  * Created by Zhivko on 2.12.2014 г..
  */
 
-jQuery(function(){
-    jQuery('.header').load('header.html');
-    jQuery('.footer').load('footer.html');
+$(document).ready(function(){
+    $('.footer').load('footer.html .footer-container');
+    $('.header').load('header.html .navbar');
+
 });
