1f98f04b763f52d6a6343989b8a3904465fc3a4b
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2014-12-08 00:39:52 +0200

    almost done routing

--- Experimental Source Code/scripts/routing.js
+++ Experimental Source Code/scripts/routing.js
@@ -27,17 +27,20 @@
         this.get('#/forum/category/:category/page/:page', function () {
             //get id and print
 
-            console.log(this.params['category']);
-            console.log(this.params['page']);
+            var categoryId = this.params['category'];
+            var page = this.params['page'];
+            //TODO: refactor
+            console.log(listItems.getQuestionsOfSpecificPageByCategory(page,2,categoryId));
             //$('#main').load('createPost.html');
         });
 
-        this.get('#/login', function () {
+        this.get('#/users', function () {
             $('#main').load('createPost.html');
         });
 
-        this.get('#/login', function () {
-            $('#main').load('createPost.html');
+        this.get('#/forum/category/:category/:question', function () {
+            var categoryId = this.params['category'];
+            var question = this.params['question'];
         });
 
         this.get('#/login', function () {
@@ -48,3 +51,74 @@
         app.run()
     });
 })(jQuery);
+
+var listItems = (function () {
+
+    var headers = $.ajaxSetup({
+        headers: {
+            "X-Parse-Application-Id": "qMgTURpDZUZXbS8OyDfvICSCMS5YkkpuMn9vIlco",
+            "X-Parse-REST-API-Key": "KOWQQ2S9fB5Hx7bNjiv4bskSQrSuZvSniHUpF4Bb"
+        }
+    });
+
+    var getCategories = function () {
+        url = 'https://api.parse.com/1/classes/category';
+        return getResultsOfSpecificPage(url);
+    };
+
+    var getUsersOfSpecificPage = function (page, resultsOfPage) {
+        var skip = (page - 1) * resultsOfPage;
+        return getResultsOfSpecificPage('https://api.parse.com/1/users?limit=' + resultsOfPage + '&skip=' + skip);
+    };
+
+    var getQuestionsOfSpecificPageByCategory = function (page, resultsOfPage, categoryId) {
+        var skip = (page - 1) * resultsOfPage;
+        url = 'https://api.parse.com/1/classes/question?where={"category":{"__type":"Pointer","className":"category","objectId":"' + categoryId + '"}}&limit=' + resultsOfPage + '&skip=' + skip;
+        return getResultsOfSpecificPage(url);
+    };
+
+    var getQuestionsOfSpecificPageByUser = function (page, resultsOfPage, userId) {
+        var skip = (page - 1) * resultsOfPage;
+        url = 'https://api.parse.com/1/classes/question?where={"author":{"__type":"Pointer","className":"_User","objectId":"' + userId + '"}}&limit=' + resultsOfPage + '&skip=' + skip;
+        return getResultsOfSpecificPage(url);
+    };
+
+    var getAnswersOfSpecificPageByQuestion = function (page, resultsOfPage, questionId) {
+        var skip = (page - 1) * resultsOfPage;
+        url = 'https://api.parse.com/1/classes/answer?where={"question":{"__type":"Pointer","className":"question","objectId":"' + questionId + '"}}&limit=' + resultsOfPage + '&skip=' + skip;
+        return getResultsOfSpecificPage(url);
+    };
+
+    var getAnswersOfSpecificPageByUser = function (page, resultsOfPage, userId) {
+        var skip = (page - 1) * resultsOfPage;
+        url = 'https://api.parse.com/1/classes/answer?where={"author":{"__type":"Pointer","className":"_User","objectId":"' + userId + '"}}&limit=' + resultsOfPage + '&skip=' + skip;
+        return getResultsOfSpecificPage(url);
+    };
+
+    var getResultsOfSpecificPage = function (url) {
+        var dataInfo = [];
+        headers;
+
+        $.ajax({
+            method: 'GET',
+            url: url,
+            async: false,
+            success: function (data) {
+                dataInfo = data.results
+            },
+
+            error: function (error) {
+                console.log(error);
+            }
+        });
+        return dataInfo
+    };
+    return {
+        getUsersOfSpecificPage: getUsersOfSpecificPage,
+        getQuestionsOfSpecificPageByCategory: getQuestionsOfSpecificPageByCategory,
+        getQuestionsOfSpecificPageByUser: getQuestionsOfSpecificPageByUser,
+        getAnswersOfSpecificPageByQuestion: getAnswersOfSpecificPageByQuestion,
+        getAnswersOfSpecificPageByUser: getAnswersOfSpecificPageByUser,
+        getCategories : getCategories
+    }
+})();
